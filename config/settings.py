1c6a45e93611d102bc3d98df3425c8a1755c9900
james <jamesjamesy75@gmail.com>
2026-09-06 13:32:44 +0330

    Security: load ALLOWED_HOSTS from DJANGO_ALLOWED_HOSTS environment variable

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", "False").lower() in ("true", "1", "yes")
 
-ALLOWED_HOSTS = []
+allowed_hosts_env = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost")
+ALLOWED_HOSTS = [host.strip() for host in allowed_hosts_env.split(",") if host.strip()]
 
 
 # Application definition
